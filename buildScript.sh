9186a1456a03226730004154f6d085b0cf7d7b99
antoine <b@b.fr>
2022-11-14 22:03:15 +0100

    update

--- buildScript.sh
+++ buildScript.sh
@@ -1,4 +1,51 @@
+#!/bin/bash
+# S1 => pattern
+# $2 => fileName
+# $3 => localFilename
+addDataToFile(){
+  echo 'if [ $(grep '"EOF_$1 $2"' |wc -l ) -eq 0 ]; then' >> template
+  echo "cat >> $2 << EOF_$1" >> template
+  echo "<<<$1>>>" >> template
+  echo "EOF_$1" >> template
+  sed -i -e "/<<<$1>>>/r $3" -e "s/<<<$1>>>//g"  template
+  echo "else" >> template
+  echo 'echo "Nothing to do"' >> template
+  echo "fi" >> template
+}
+addTestUserAsRoot(){
+  echo 'if (whoami != root)' >> template
+  echo 'then echo "Please run as root"' >> template
+  echo 'exit 1' >> template
+  echo 'fi' >> template
+}
+
+echo "#!/bin/bash" > template
+echo "#!/bin/bash" > out.sh
+addTestUserAsRoot
+addDataToFile PAT1 "/root/.tmux.conf" tmuxFile
+addDataToFile PAT2 "/root/.vimrc" vimrcFile
+addDataToFile PAT3 "/root/.bashrc" bashrcFile
+addDataToFile PAT4 "/root/.vim/coc-settings.json" coc-settings.json
 
 
-cat template |sed -e "/<<<PAT>>>/r bashrcFile" -e "s/<<<PAT>>>//g" -e "/<<<PAT2>>>/r tmuxFile" -e "s/<<<PAT2>>>//g" > out.sh
-bash out.sh
+echo "apt update && apt install git curl make clang libtool-bin python3-dev python3-pip exuberant-ctags python3-venv golang-go ranger -y" >> template
+echo "pip3 install jedi && pip3 install console_colors && git clone https://github.com/vim/vim.git" >> template
+echo "curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" >> template
+echo "/bin/bash -c \"cat <(echo 'FORCE'='yes') <(curl -sL install-node.now.sh/lts) |bash\"" >> template
+echo "cd vim && ./configure --enable-python3interp && cd src && make && make install" >> template
+#COPY vimrcFile /root/.vimrc
+echo "/bin/bash -c 'echo \"q\" |vim  +PlugInstall +qall || true'" >> template
+echo "npm install --global yarn && cd /root/.vim/plugged/coc.nvim/ && yarn install" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-css' +qall\""  >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-json' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-yaml' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-python' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-git' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-docker' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-cmake' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-xml' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-go' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-sh' +qall\"" >> template
+echo "/bin/bash -c \"vim +'CocInstall -sync coc-snippets' +qall\"" >> template
+echo "/bin/bash -c \"echo 'export GOPATH=$HOME/go' >> /root/.bash_profile\"" >> template
+echo "/bin/bash -c \"echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> /root/.bash_profile\"" >> template
