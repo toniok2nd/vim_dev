954954e7e897b749b5b31170d2757855df49a930
toniok <92204802+toniok2nd@users.noreply.github.com>
2025-01-09 23:30:39 +0100

    Create vim_alpine.sh

---
+++ vim_alpine.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Function to check if the system is Alpine
+is_alpine() {
+    if grep -iq "alpine" /etc/os-release; then
+        return 0  # true (Alpine)
+    else
+        return 1  # false (not Alpine)
+    fi
+}
+
+if is_alpine; then
+
+  # install package
+        apk update
+  apk add --no-cache python3-dev
+  apk add --no-cache build-base ncurses-dev 
+        apk add --no-cache curl jq yq git fzf tmux bash tmux go nodejs vim npm
+
+  # set venv
+  python3 -m venv /VENV
+  source /VENV/bin/activate
+
+        # Download and install pip using get-pip.py
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+        python3 get-pip.py
+  
+  # add configuration
+  curl https://raw.githubusercontent.com/toniok2nd/vim_dev/master/vimrcFile -o ~/.vimrc
+  curl https://raw.githubusercontent.com/toniok2nd/vim_dev/master/tmuxFile -o ~/.tmux.conf
+
+  # install vim plugin
+  /bin/bash -c 'echo \"q\" |vim +PlugInstall +qall || true'
+  npm install --global yarn && cd /root/.vim/plugged/coc.nvim/ && yarn install
+
+fi
