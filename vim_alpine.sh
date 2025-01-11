aa13eee737c97c391caca5bdcb9015177c2dbae2
toniok <92204802+toniok2nd@users.noreply.github.com>
2025-01-11 17:37:22 +0100

    Update vim_alpine.sh

--- vim_alpine.sh
+++ vim_alpine.sh
@@ -28,6 +28,7 @@ if is_alpine; then
   # add configuration
   curl https://raw.githubusercontent.com/toniok2nd/vim_dev/master/vimrcFile -o ~/.vimrc
   curl https://raw.githubusercontent.com/toniok2nd/vim_dev/master/tmuxFile -o ~/.tmux.conf
+  curl https://raw.githubusercontent.com/toniok2nd/vim_dev/master/bashFile -o ~/.bashrc
 
   # install vim plugin
   /bin/bash -c 'echo \"q\" |vim +PlugInstall +qall || true'
